c1542cca467583a721e78a226091cb3c8d7c7068
MirkoValle <mirko.valle20@gmail.com>
2024-05-07 16:21:54 +0200

    HTML + JS

---
+++ script.js
@@ -0,0 +1,5 @@
+const userName = prompt("Insert your name")
+const userLastname = prompt("Insert your Lastname")
+const userFavouriteColor = prompt("Insert your favourite color")
+
+document.getElementById("result").innerHTML = userName + userLastname + userFavouriteColor + "24"
